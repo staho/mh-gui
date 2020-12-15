9cb02c250c19c1f4a02b4e6962955fadbcbb3b88
staho <cSa5PmtR8t88R5a>
2020-12-15 19:34:01 +0100

    Views are changing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,65 @@
 import './App.css';
 import { Grid } from '@material-ui/core';
+import React from 'react'
 import AppBar from './Components/AppBar';
 import Parameters from './Components/Parameters';
+import Data from './Components/Data';
+import Result from './Components/Result';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <Grid
-            style={{height: "100vh"}}
-            container
-            direction="column"
-            justify="flex-start"
-            alignItems="stretch">
-              <Grid item>
-                <AppBar>
+class App extends React.Component {
 
-                </AppBar>
-              </Grid>
-              <Grid item style={{height: "90vh"}} container direction="row" justify="center" alignItems="center">
-                <Parameters />
-              </Grid>
-        </Grid>
-      </header>
-    </div>
-  );
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentStep: null
+    }
+  }
+
+  handleStepChange = step => {
+    console.log(step)
+    this.setState({currentStep: step})
+  }
+
+  resolveView = currentStep => {
+    if(currentStep != null) {
+      currentStep = currentStep.key
+    }
+    switch (currentStep) {
+      case "data":
+        return <Data />
+      case "result":
+        return <Result />
+      case "parameters":
+      default:
+        return <Parameters />
+    }
+  }
+
+  render() {
+    const currentStep = this.state.currentStep
+    const stepView = this.resolveView(currentStep)
+
+    return (
+      <div className="App">
+        <header className="App-header">
+            <Grid
+              style={{height: "100vh"}}
+              container
+              direction="column"
+              justify="flex-start"
+              alignItems="stretch">
+                <Grid item>
+                  <AppBar handleStepChange={this.handleStepChange} />
+                </Grid>
+                <Grid item style={{height: "90vh"}} container direction="row" justify="center" alignItems="center">
+                  {stepView}
+                </Grid>
+          </Grid>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/Components/AppBar.js
+++ src/Components/AppBar.js
@@ -2,26 +2,53 @@ import { Grid, Step, StepLabel, Stepper } from '@material-ui/core'
 import App from '../App'
 import React from 'react'
 
+const steps = [
+    {
+        label: "Parametry",
+        key: "parameters"
+    },
+    {
+        label: "Dane",
+        key: "data"
+
+    },
+    {
+        label: "Wynik",
+        key: "result"
+    }
+]
+
 class AppBar extends React.Component {
+    constructor(props) {
+        super(props) 
+        this.state = {
+            currentStep: 0
+        }
+    }
+
+    handleStep = step => () => {
+        this.props.handleStepChange(steps[step])
+        this.setState({currentStep: step})
+    }
+
+    prepareSteps = () => {
+        return steps.map((step, i) => {
+            return (
+                <Step key={step.key}>
+                    <StepLabel onClick={this.handleStep(i)}>
+                        {step.label}
+                    </StepLabel>
+                </Step>
+            )
+        })
+    }
 
     render() {
+        const steps = this.prepareSteps()
+
         return(
-            <Stepper>
-                <Step>
-                    <StepLabel>
-                        Parametry
-                    </StepLabel>
-                </Step>
-                <Step>
-                    <StepLabel>
-                        Dane
-                    </StepLabel>
-                </Step>
-                <Step>
-                    <StepLabel>
-                        Wynik
-                    </StepLabel>
-                </Step>
+            <Stepper activeStep={this.state.currentStep}>
+                {steps}
             </Stepper>
         )
     }
