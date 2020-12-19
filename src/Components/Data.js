b78ee3aee4a12f77968db6df38f54f9373a43627
staho <cSa5PmtR8t88R5a>
2020-12-19 11:40:24 +0100

    Small changes

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 class Data extends React.Component {
 
+    prepareEmptyRows = noOfRows => {
+        let rows = []
+        for(let i = 0; i < noOfRows; i++) {
+            rows.push(
+                
+            )
+        }
+    }
+
     render() {
         return(
             <div>
