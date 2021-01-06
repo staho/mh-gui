d180b5d09449ca51ec6ed7f2fc2bd339d96f8b31
staho <cSa5PmtR8t88R5a>
2021-01-06 12:15:43 +0100

    Added details to result display

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -1,9 +1,11 @@
+import { Collapse, List, ListItem, ListItemText, Paper } from '@material-ui/core'
+import { ExpandLess, ExpandMore } from '@material-ui/icons'
 import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet'
 import { WAYS } from './../DevData/ResultMock'
 
 
-const colors = ["#FF5733", "#000000", "#0017FF", "#FF00D8", "#7800FF", "#905C1E", "#FF4BAD", "#006114", "#AF601A", "#B03A2E" ]
+const colors = ["#FF5733", "#000000", "#0017FF", "#FF00D8", "#7800FF", "#905C1E", "#FF4BAD", "#006114", "#AF601A", "#B03A2E"]
 
 class Result extends React.Component {
     constructor(props) {
@@ -26,7 +28,7 @@ class Result extends React.Component {
     preparePolyLines = ways => {
         const lines = this.prepareLines(ways)
         return lines.map((line, i) => {
-            return <Polyline positions={line} pathOptions={{color: colors[i]}}/>
+            return <Polyline positions={line} pathOptions={{ color: colors[i] }} />
         })
     }
 
@@ -35,10 +37,10 @@ class Result extends React.Component {
     }
 
     prepareSingleMarker = markerData => {
-        const position = [ markerData.lat, markerData.lon ]
-        return <Marker position = {position}>
+        const position = [markerData.lat, markerData.lon]
+        return <Marker position={position}>
             <Popup>
-                {`${markerData.city}`}<br/>
+                {`${markerData.city}`}<br />
                 {`Demand: ${markerData.demand}`}
             </Popup>
         </Marker>
@@ -48,10 +50,49 @@ class Result extends React.Component {
         var letters = '0123456789ABCDEF'
         var color = '#'
         for (var i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)]
+            color += letters[Math.floor(Math.random() * 16)]
         }
-        return {color: color}
-      }
+        return { color: color }
+    }
+
+    preparePathListItem = index => {
+        return <ListItem button onClick={this.handlePathClick(index)}>
+                    <ListItemText style={{ color: "black" }} primary={`Scieżka ${index + 1}`} />
+                    {this.state[`expanded${index}`] ? <ExpandLess style={{color: "black"}} /> : <ExpandMore style={{color: "black"}}/>}
+                </ListItem>
+    }
+
+    prepareHiddenPathList = (path, index) => {
+        console.log(path)
+        let items = path.map(city => {
+            return <ListItem style={{paddingLeft: "40px"}}>
+                        <ListItemText style={{ color: "black" }} primary={`${city.name}`} secondary={city.demand === 0 ? "Depot" : ''} />
+                    </ListItem>
+        })
+
+        return <Collapse in={this.state[`expanded${index}`]} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        {items}
+                    </List>
+                </Collapse>
+    }
+
+    preparePathsAsLists = paths => {
+        let elements = []
+        for(let i = 0; i < paths.length; i++) {
+            elements.push(this.preparePathListItem(i))
+            elements.push(this.prepareHiddenPathList(paths[i].pathCities, i))
+        }
+        
+        return elements
+    }
+
+    handlePathClick = option => () => {
+        let open = this.state[`expanded${option}`]
+        open = open === undefined ? false : open
+        open = !open
+        this.setState({[`expanded${option}`]: open})
+    }
 
     render() {
         const result = this.props.result
@@ -59,17 +100,38 @@ class Result extends React.Component {
         const paths = result.paths
         const markers = this.prepareMarkers(data)
         const polyLines = this.preparePolyLines(paths)
+        const listElements = this.preparePathsAsLists(paths)
 
         return (
-            <div style={{height: "100%", width: "100%"}}>
-                <MapContainer center={[52.082, 19.48]} zoom={7} scrollWheelZoom={false} style={{height: "inherit", width:"inherit"}}>
-                    <TileLayer
-                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    {polyLines}  
-                    {markers}                  
-                </MapContainer>
+            <div style={{ height: "100%", width: "100%", display: "flex" }}>
+                <div style={{ height: "100%", width: "70%" }}>
+                    <MapContainer center={[52.082, 19.48]} zoom={7} scrollWheelZoom={false} style={{ height: "100%", width: "100%" }}>
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        {polyLines}
+                        {markers}
+                    </MapContainer>
+                </div>
+                <div style={{ height: "100%", width: "30%" }}>
+                    <Paper elevation={0} style={{ fontSize: "24px" }}>{`Koszt całkowity: ${result.cost}`}</Paper>
+                    <Paper elevation={0} style={{ fontSize: "24px" }}>{`Czas przebiegu algorytmu [s]: ${result.executionTime}`}</Paper>
+                    <List>
+                        {listElements}
+                        {/* <ListItem button onClick={this.handlePathClick(0)}>
+                            <ListItemText style={{ color: "black" }} primary="Wynik 1" />
+                        </ListItem>
+                        <Collapse in={false} timeout="auto" unmountOnExit>
+                            <List component="div" disablePadding>
+                                <ListItem button style={{paddingLeft: "14px"}}>
+                                    <ListItemText primary="Starrd" />
+                                </ListItem>
+                            </List>
+                        </Collapse> */}
+                    </List>
+                </div>
+
             </div>
         )
     }
